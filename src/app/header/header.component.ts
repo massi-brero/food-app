c37258251f7dc226b4df233f8ae02f1e3574f142
massi.brero@gmail.com <massi.brero@gmail.com>
2022-04-15 15:29:41 +0200

    auth interceptor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import { DataStorageService } from '../shared/services/data-storage.service'
 import { AuthService } from '../auth/services/auth.service'
+import {Subscription} from 'rxjs'
+import {User} from '../auth/models/user'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false
+  private subscription: Subscription
+
   constructor(
     private dataStorageService: DataStorageService,
     private authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.authService.user$.subscribe((user: User)  => {
+      this.isAuthenticated = !!user
+    })
+  }
 
   onSave() {
     this.dataStorageService.save()
@@ -21,5 +30,9 @@ export class HeaderComponent implements OnInit {
 
   onFetchAll() {
     this.dataStorageService.fetchAll().subscribe()
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 }
--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import {HttpClient, HttpParams} from '@angular/common/http'
 import { RecipeService } from '../../recipes/services/recipe.service'
 import { environment } from '../../../environments/environment'
 import { Recipe } from '../../recipes/recipe.model'
-import { map, tap } from 'rxjs/operators'
-import {Observable} from 'rxjs'
+import { map, tap, take, exhaustMap } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+import { AuthService } from '../../auth/services/auth.service'
+import { User } from '../../auth/models/user'
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   save(): void {
     const recipes = this.recipeService.allRecipes
@@ -20,9 +26,20 @@ export class DataStorageService {
         console.log(response)
       })
   }
-
+  // .subscribe((user: User) => {
+  //
+  // })
   fetchAll(): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(`${environment.apiUrl}/recipes.json`).pipe(
+    return this.authService.user$.pipe(
+      take(1),
+      exhaustMap((user: User) => {
+        return this.http.get<Recipe[]>(
+          `${environment.apiUrl}/recipes.json`,
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        )
+      }),
       map((recipes) => {
         return recipes.map((recipe) => {
           return {
