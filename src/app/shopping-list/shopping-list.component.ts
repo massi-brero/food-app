22aeb243a28579424afa9db39276d4eab67714c3
massi.brero@gmail.com <massi.brero@gmail.com>
2022-06-04 23:47:58 +0200

    ngrx 1

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,37 +1,39 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Ingredient} from "../shared/ingredient.model";
-import {ShoppingListService} from "./services/shopping-list.service";
-import {Subscription} from "rxjs";
-import {LoggingService} from '../logging.service'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './services/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
-  private igChangedSub: Subscription;
+  ingredients: Observable<{ ingredients: Ingredient[] }>
+  // private igChangedSub: Subscription
 
   constructor(
     private slService: ShoppingListService,
-    private loggingService: LoggingService
-  ) { }
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
   ngOnDestroy(): void {
-        this.igChangedSub.unsubscribe();
-    }
+    // this.igChangedSub.unsubscribe()
+  }
 
   ngOnInit(): void {
-    this.ingredients = this.slService.allIngredients;
-    this.igChangedSub = this.slService.$ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients
-      }
-    )
+    this.ingredients = this.store.select('shoppingList')
+    // this.ingredients = this.slService.allIngredients
+    // this.igChangedSub = this.slService.$ingredientsChanged.subscribe(
+    //   (ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients
+    //   }
+    // )
   }
 
   onEditItem(idx: number) {
-    this.slService.startedEditing.next(idx);
+    this.slService.startedEditing.next(idx)
   }
 }
